8b9e05b59df91cd9804cbc832d8ae64a07c5fa5d
Katorin <56090625+thevuong8000@users.noreply.github.com>
2020-06-15 08:42:16 -0700

    update Lina
    She can also search for word's meanings by dictionary, find location and search on wikipedia

--- Lina.py
+++ Lina.py
@@ -4,15 +4,21 @@ import json
 import random
 import playsound
 import os
+import warnings
+import wikipedia
+import webbrowser
 
 from datetime import date, datetime
 from darksky.api import DarkSky
 from darksky.types import languages, units, weather
 from gtts import gTTS 
+from PyDictionary import PyDictionary
 
 # 1.Initialize
 speech = sr.Recognizer() # recorder
 today = date.today() # current date
+dictionary = PyDictionary() # dictionary
+warnings.filterwarnings('ignore') # ignore warnings
 
 # init robot_brain
 with open("knowledge.JSON") as file:
@@ -38,9 +44,9 @@ forecast = darksky.get_forecast( # get forecast for Hanoi
 
 
 # 2.Define fundamental function
-def getRequest(): # record user's voice
+def getRequest(ask): # record user's voice
 	with sr.Microphone() as mic:
-		print("Say something!")
+		print(ask)
 		audio = speech.listen(mic)
 
 	try:
@@ -49,11 +55,11 @@ def getRequest(): # record user's voice
 		return "" 
 
 def verbalize(res): # robot responses by speaking
-	print("Pending...")
 	myobj = gTTS(text=res, lang='en', slow=False) 
 	myobj.save("output.mp3") # Convert text to speech and save as mp3 file
 	print("robot: " + robot_response)
 	playsound.playsound('output.mp3') # Play the converted file 
+	os.remove("output.mp3")
 
 def checkPattern(req): # check if robot can answer this question base on "hard" knowledge
 	for intent in robot_brain['intents']:
@@ -66,14 +72,16 @@ def checkPattern(req): # check if robot can answer this question base on "hard" 
 # 3.Make conversation
 while True:
 	# obtain audio from the microphone
-	you = getRequest() # catch voice
+	you = getRequest("Say something!!!") # catch voice
 	print("You said: " + you)
 
 	# robot's response
 	if you == "":
 		robot_response = "I can't hear you! Please try again"
+	
 	elif you == "ok" or you == "okay" or you == "okay fine":
 		robot_response = "okay!"
+	
 	elif "today" in you:
 		if "weather" in you:
 			robot_response = "Current temperature is " + str(forecast.currently.temperature) + "\n"
@@ -81,8 +89,46 @@ while True:
 			robot_response += "And this week will be " + forecast.daily.summary
 		else:
 			robot_response = "Today is " + today.strftime("%B %d, %Y")
+	
 	elif "time" in you:
 		robot_response = datetime_Hanoi.strftime("%H hours %M minutes %S seconds")
+	
+	elif "what does" in you and "mean" in you:
+		wordSpl = you.split(' ')
+		word = dictionary.meaning(wordSpl[-2])
+		if word is None:
+			robot_response = "I can't not find it in the dictionary"
+		else:
+			robot_response = "Check out what I have found"
+			for wordType, meanings in word.items():
+				print(wordType + ": ")
+				for meaning in meanings:
+					print("    - " + meaning)
+	
+	elif "search for" in you:
+		search = you.split('search for ')[-1]
+		try:
+			overallInfo = wikipedia.summary(search, sentences=2)
+			print(overallInfo)
+			print("URL: " + wikipedia.page(search).url)
+			robot_response = "Here's what I found"
+		except:
+			robot_response = "I can't find any information related to " + search + " with Wikipedia! Try to search it yourself."
+	
+	elif "find location of" in you:
+		place = you.split("find location of ")[-1]
+		url = "http://google.nl/maps/place/" + place + "/&amp"
+		webbrowser.get().open(url)
+		robot_response = "Here's where I found for " + place
+	
+	elif "stop" in you:
+		verbalize("Okay! Bye")
+		break
+	
+	elif "thank you" in you or "thanks" in you:
+		ans = ["No problem!", "You're welcome", "I'm glad to help you!"]
+		robot_response = random.choice(ans)
+	
 	else:
 		intent = checkPattern(you)
 		robot_response = random.choice(intent['responses'])
@@ -91,7 +137,5 @@ while True:
 			break
 
 	# verbal response
+	print("Pending...")
 	verbalize(robot_response) # robot speaks
-
-if os.path.exists("output.mp3"):
-	os.remove("output.mp3")
